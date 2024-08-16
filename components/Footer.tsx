8114aca9b2bd65873b2db3d41469a2e05d179a3a
kkian481718 <kkian481718@google.com>
2024-08-16 12:45:40 +0800

    modified:   components/Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,8 @@ export default function Footer() {
           <SocialIcon kind="threads" href={siteMetadata.threads} size={6} /> */}
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{siteMetadata.author} {`© ${new Date().getFullYear()}`}</div>
+          <div>{siteMetadata.author}</div>
+          <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
