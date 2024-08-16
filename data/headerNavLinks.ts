4a32afba26e1632ee0096757802808861ddf2bd7
kkian481718 <kkian481718@google.com>
2024-08-16 14:45:16 +0800

    change lots of things

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  { href: '/blog', title: '所有文章' },
+  { href: '/tags', title: 'Tags列表' },
+  // { href: '/projects', title: 'Projects' },
   { href: '/about', title: '關於我們' },
 ]
 
